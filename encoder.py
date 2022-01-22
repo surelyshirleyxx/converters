33beb0097c8d38926e486a6a3683d0b4c062c596
shirleyxx <74038718+surelyshirleyxx@users.noreply.github.com>
2022-01-22 00:34:36 -0700

    testing

--- encoder.py
+++ encoder.py
@@ -9,28 +9,24 @@ CONSONANTS = "".join(CONSONANTS)
 
 
 def convert(us):  # us = user_string
-    word = us
-    word = word.lower().split()
-    for i, word in enumerate(word):
-
-    # take the first letter and move to end
-    # attach "ay" and bob's your uncle
+    for word in us.split():
+        # take the first letter and move to end
+        # attach "ay" and bob's your uncle
         if word[0] in CONSONANTS and word[1] in CONSONANTS:
             # when the first two letters are consonants
-            word = word[2:] + word[0] + word[1] + 'ay'
+            new_word = word[2:] + word[0] + word[1] + 'ay'
         elif word[0] in CONSONANTS:
             # when only the first letter is a consonant
-            word = word[1:] + word[0] + 'ay'
+            new_word = word[1:] + word[0] + 'ay'
         else:
-            word = word[0] in VOWELS
+            # word = word[0] in VOWELS
             # when the first letter is a vowel
-            word = word[0:] + 'ay'
-
-    output = word
-    " ".join(output)
+            new_word = word[0:] + 'ay'
 
     # " ".join(['apple', 'bat', 'copy'])
+    output = " ".join(new_word)
 
     if output == "igpay":
         return "hi!"
+
     return output
