50b8810aa6bc813b5c7d2453b7400e224598d73f
shirleyxx <74038718+surelyshirleyxx@users.noreply.github.com>
2022-01-23 18:40:32 -0700

    testing

--- encoder.py
+++ encoder.py
@@ -13,15 +13,15 @@ def convert(us):  # us = user_string
     # take the first letter and move to end
     # attach "ay" and bob's your uncle
     for i, word in enumerate(words):
-        for j, letter in enumerate(word):
-            if letter[0] in VOWELS:
+        for j, _ in enumerate(word):
+            if word[0] in VOWELS:
                 # when the first letter in each word is a vowel
                 words[i] = word[0:] + "ay"
-            elif letter[0] in CONSONANTS:
+            elif word[0] and word[1] in CONSONANTS:
+                words[i] = word[2:] + word[0] + word[1] + "ay"
+            elif word[0] in CONSONANTS:
                 # when first letter in each word is a consonant
                 words[i] = word[1:] + word[0] + "ay"
-            elif letter[0] and letter[1] in CONSONANTS:
-                words[i] = word[2:] + word[0] + word[1] + "ay"
             break
 
     # " ".join(['apple', 'bat', 'copy'])
